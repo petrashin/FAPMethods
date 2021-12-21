ed756729c32c8b7b71e01e583c02e038ba8d469f
Maxlin12 <maxim.linetskiy@gmail.com>
2021-12-22 00:37:47 +0300

    Самса

--- dirivative_int.py
+++ dirivative_int.py
@@ -2,15 +2,35 @@ from numpy import arange, isinf, isnan
 
 
 def func(function):
+    """
+    Функция, которая выполняет переданную функцию
+    :params function: передается строка с функцией
+    :return: возвращает выполненную строку с функцией
+    """
     return eval(function)
 
 
 def findDerivative(function, left_bound, right_bound, step):
+    """
+    Функция - генератор поиска производной
+    :params function: передается строка с функцией
+    :params left_bound, right_bound: левая и правая границы
+    :params step: шаг
+    :return: иттерационно возвращает точки в указанном интервале
+    """
     for i in arange(left_bound, right_bound, step):
         yield (function(i + step) - function(i - step)) / (2 * step)
 
 
 def findIntegration(function, left_bound, right_bound, step, except_points):
+    """
+    Функция поиска интеграла
+    :params function: передается строка с функцией
+    :params left_bound, right_bound: левая и правая границы
+    :params step: шаг
+    :params except_points: точки, где функция не существует
+    :return: иттерационно возвращает точки в указанном интервале
+    """
     summ = 0
     for i in arange(left_bound, right_bound, step):
         if round(i, 5) not in except_points:
@@ -21,6 +41,12 @@ def findIntegration(function, left_bound, right_bound, step, except_points):
 
 
 def findExceptions(function, left_bound, right_bound):
+    """
+    Функция поиска точек, где функция не существует
+    :params function: передается строка с функцией
+    :params left_bound, right_bound: левая и правая границы
+    :return: возвращает точки, где функция не существует
+    """
     results = []
     for i in arange(round(left_bound, 2), round(right_bound, 2), 1e-2):
         fx = function(round(i, 2))
@@ -30,6 +56,13 @@ def findExceptions(function, left_bound, right_bound):
 
 
 def my_differentiation(left_bound, right_bound, step):
+    """
+    Функция - генератор поиска производной
+    :params function: передается строка с функцией
+    :params left_bound, right_bound: левая и правая границы
+    :params step: шаг
+    :return: списки точек в указанном интервле
+    """
     list_of_iterations, list_of_x_values, list_of_diff_values = [], [], []
     counter = 0
     for x in arange(left_bound, right_bound + step, step):
