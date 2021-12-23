606322ae1a405522895632836867636c80d06f1a
George Danilov <tiazkezo@gmail.com>
2021-12-23 13:57:25 +0300

    setup.py for lib

---
+++ build/lib/fapmethods/optimize.py
@@ -0,0 +1,424 @@
+import numpy as np
+from random import randint
+from collections import namedtuple
+import pandas as pd
+from numpy.random import choice as np_choice
+import time
+
+
+def timer(func):
+    """
+    Декоратор, выводящий время которое заняло
+    выполнение декорируемой функции
+    """
+
+    def wrapped_func(*args, **kwargs):
+        started_at = time.time()
+        result = func(*args, **kwargs)
+        ended_at = time.time()
+        run_time = round(ended_at - started_at, 4)
+        return *result, run_time
+    return wrapped_func
+
+
+class TSP:
+
+    def __init__(self, matrix):
+        self.__matrix = matrix
+
+    def __repr__(self):
+        return repr(self.matrix)
+
+    @property
+    def n(self):
+        """
+        Возвращает число - количество вершин.
+        :params: self
+        :return: количество вершин графа.
+        """
+        return self.matrix.shape[0]
+
+    @property
+    def matrix(self):
+        """
+        Матрица весов
+        :params: self
+        :return: копия матрицы весов
+        """
+        return self.__matrix.copy()
+
+    @classmethod
+    def random(cls, n, bottom_limit=1, upper_limit=10, orientated=False):
+        """
+        Метод в классе, генерирующий связанный граф со случайными весами
+        :params: n - размерность
+        :params: bottom_limit - минимальный вес ребра
+        :params: upper_limit - максимальный вес ребра
+        :params: orientated - ориентированный ли граф
+        :return: Возвращается созданный объект класса
+        """
+        # method = uniform
+        method = randint
+        matrix = np.empty((n, n))
+
+        if orientated:
+            for i in range(n):
+                for j in range(n):
+                    if i == j:
+                        matrix[i][j] = np.Inf
+                    else:
+                        matrix[i][j] = method(bottom_limit, upper_limit)
+
+        else:
+            for i in range(n):
+                for j in range(i, n):
+                    if i == j:
+                        matrix[i][j] = np.Inf
+                    else:
+                        matrix[i][j] = method(bottom_limit, upper_limit)
+                        matrix[j][i] = matrix[i][j]
+
+        return cls(matrix)
+
+    @classmethod
+    def keybord_input(cls, n):
+        """
+        Метод в классе, позволяющий ввести матрицу с клавиатуры
+        :params: n - размерность
+        :return: Возвращается созданный объект класса
+        """
+        matrix = np.empty((n, n))
+        for i in range(n):
+            for j in range(n):
+                if i == j:
+                    matrix[i][j] = np.Inf
+                else:
+                    matrix[i][j] = float(input())
+
+        return cls(matrix)
+
+    @timer
+    def branchNBound(self):
+        """
+        Функция, реализующая метод ветвей и границ
+        :params: self
+        :return: вычисленный итоговый путь + его длина
+        """
+        n_rows = self.matrix.shape[0]
+        n_cols = self.matrix.shape[1]
+        table = pd.DataFrame(self.matrix,
+                             columns=list(range(n_cols)),
+                             index=list(range(n_rows)))
+        leaf = namedtuple('Leaf', ['table',
+                                   'bottom_bound',
+                                   'included',
+                                   'edge'
+                                   ]
+                          )
+
+        def path(edges):
+            """
+            Функция нахождения пути
+            :params: edges like list (?)
+            :return: найденный list
+            """
+            edges = dict(edges)
+            v_set = set(edges.keys())
+            lst = []
+            loop_flag = False
+            while len(v_set) > 0:
+                lst.append([v_set.pop()])
+                while True:
+                    try:
+                        v = edges[lst[-1][-1]]
+                        v_set -= {v}
+                        lst[-1].append(v)
+                        if lst[-1][0] == lst[-1][-1]:
+                            loop_flag = True
+                            break
+                    except KeyError:
+                        break
+            if loop_flag and len(lst) > 1:
+                return []
+            return lst[0]
+
+        def score_func(table_):
+            """
+            Целевая функция
+            :params: table_ таблица весов
+            :return: приведенная таблица весов, нижняя границу
+            """
+            assert table_.shape[0] > 0
+            table = table_.copy()
+
+            matrix = np.array(table)
+
+            bottom_bound = 0
+            for i in range(matrix.shape[0]):
+                min_ = min(matrix[i])
+                if min_ == np.inf:
+                    return None, np.inf
+                bottom_bound += min_
+                matrix[i] -= min_
+            for i in range(matrix.shape[1]):
+                min_ = min(matrix[:, i])
+                if min_ == np.inf:
+                    return None, np.inf
+                bottom_bound += min_
+                matrix[:, i] -= min_
+
+            table[:][:] = matrix
+            assert table.shape[0] > 0
+            return table, bottom_bound
+
+        def max_penalty(table):
+            """
+            функция нахождения самого "тяжелого" нуля
+            :params: table (см. выше)
+            :return: ребра с самым тежелым ребром
+            """
+            assert table.shape[0] > 0
+            max_zero = 0  # самый тяжелый ноль
+            max_zero_edge = (0, 0)
+            matrix = np.array(table.copy())
+
+            for i, j in zip(*np.where(matrix == 0)):  # находим индексы нулей
+                matrix[i, j] = np.Inf
+                assert min(matrix[i]) >= 0 and min(matrix[j]) >= 0
+                d = min(matrix[i]) + min(matrix[j])
+                if d >= max_zero:
+                    max_zero = d
+                    max_zero_edge = (i, j)
+                matrix[i, j] = 0
+
+            max_zero_edge = (table.index[max_zero_edge[0]],
+                             table.columns[max_zero_edge[1]])
+            return max_zero_edge
+
+        def include(leaf_):
+            """
+            функция включения ребра в путь
+            :params: объект содержащий матрицу весов, ребро для включения, включенние ребра, нижнюю границу
+            :return: объект с включенним ребром
+            """
+            assert leaf_.table.shape[0] > 0
+            table = leaf_.table.copy()
+            i, j = leaf_.edge
+            table.drop(i, 'index', inplace=True)  # вычеркиваем строку
+            table.drop(j, 'columns', inplace=True)  # и столбец
+            table, bottom_bound = score_func(table)  # приведение матрицы
+            # сложение нижней границы и константы приведения
+            bottom_bound += leaf_.bottom_bound
+            # добавляем старое ребро в путь
+            included = leaf_.included + [leaf_.edge]
+            if bottom_bound == np.inf:
+                return None
+            edge = max_penalty(table)[0]  # ищем следующее ребро
+            # путь замкнулся раньше времени
+            if len(path(included + [edge])) == 0:
+                return None
+            return leaf(table, bottom_bound, included, edge)
+
+        def exclude(leaf_):
+            """
+            функция исключения ребра из пути
+            :params: объект содержащий матрицу весов, ребро для исключения, включенные ребра, нижнюю границу
+            :return: объект с включенним ребром
+            """
+
+            assert leaf_.table.shape[0] > 0
+            table = leaf_.table.copy()
+            i, j = leaf_.edge
+            table.loc[i, j] = np.Inf
+            table, bottom_bound = score_func(table)  # приведение матрицы
+            # сложение нижней границы и константы приведения
+            bottom_bound += leaf_.bottom_bound
+            if bottom_bound == np.inf:
+                return None
+            included = leaf_.included  # оставляем путь таким же
+            edge = max_penalty(table)[0]  # ищем следующее ребро
+            return leaf(table, bottom_bound, included, edge)
+
+        table_, bottom_bound_ = score_func(table)
+        edge = max_penalty(table_)[0]
+        tree_leaves = [leaf(table_, bottom_bound_, [], edge)]
+        ii = 0
+        while True:
+            ii += 1
+            # эвристика выбора следующего ребра
+            tree_leaves = sorted(tree_leaves, key=lambda x: x.bottom_bound)
+            min_leaf_check = min_leaf = tree_leaves[0]
+            min_leaf_index = 0
+            for i, l in enumerate(tree_leaves):
+                if min_leaf.bottom_bound != l.bottom_bound:
+                    break
+                if l.table.shape[0] < min_leaf.table.shape[0]:
+                    min_leaf_index = i
+
+            # деление на включенный и запрещенный
+            min_leaf = tree_leaves.pop(min_leaf_index)
+
+            # условие остановки
+            if min_leaf.table.shape[0] == 1:
+                break
+            temp = include(min_leaf)
+            if temp is not None:
+                tree_leaves.append(temp)
+            temp = exclude(min_leaf)
+            if temp is not None:
+                tree_leaves.append(temp)
+
+        return path(min_leaf.included + [min_leaf.edge]), min_leaf.bottom_bound, ii
+
+    def countPathCost(self, path):
+        """
+        функция поиска "стоимости" пути
+        :params: путь, найденный выше
+        :return: путь, стоимость пути
+        """
+        c = 0
+        i = 0
+        try:
+            for i in range(1, len(path)):
+                # print(path[i-1], path[i])
+                c += self.matrix[path[i - 1]][path[i]]
+        except:
+            print(path[i - 1], [path[i]])
+        return c
+
+    @timer
+    def Boltsman(self, t0=1000):
+        """
+        Реализация метода имитации Больцмановского отжига
+        :params: t0 начальная температура
+        :return: стоимость пути (число)
+        """
+        def t(k):
+            return t0 / (k**0.8)
+
+        def isTransit(x0, x, ct):
+            df = self.countPathCost(x) - self.countPathCost(x0)
+            if df <= 0:
+                return True
+            else:
+                if np.exp(-df / ct) >= np.random.uniform():
+                    return True
+
+                return False
+
+        def getNewState(x):
+            x = x.copy()
+            r1 = np.random.randint(0, len(x) - 2)
+            r2 = np.random.randint(0, len(x) - 2)
+            while r1 == r2:
+                r2 = np.random.randint(0, len(x) - 1)
+            x[r1], x[r2] = x[r2], x[r1]
+            x[-1] = x[0]
+            return x
+
+        ct = t(1)
+        x = [i for i in range(0, len(self.matrix))] + [0]
+
+        counter = 1
+
+        while counter <= 10 ** 4:
+            x_new = getNewState(x)
+            if isTransit(x, x_new, ct):
+                x = x_new
+            counter += 1
+            ct = t(counter)
+        return x, self.countPathCost(x)
+
+    @timer
+    def AntColonyMethod(self, n_ants=None, n_best=5, n_iterations=None):
+        """
+        Реализация Муравьиного алгоритма
+        :params: кол-во муравьев
+        :params:
+        :params: кол-во итераций
+
+        :return: кратчайший цикл, длина цикла
+        """
+        if n_ants:
+            n_ants = self.n
+        if n_iterations:
+            n_iterations = self.n * 4
+        decay = 0.95
+        alpha = 1
+        beta = 1
+
+        class AntColony:
+            def __init__(self, distances, n_ants, n_best, n_iterations, decay, alpha, beta):
+
+                self.distances = distances
+                self.pheromone = np.ones(
+                    self.distances.shape) / len(self.distances)
+                self.all_inds = range(len(self.distances))
+                self.n_ants = n_ants
+                self.n_best = n_best
+                self.n_iterations = n_iterations
+                self.decay = decay
+                self.alpha = alpha
+                self.beta = beta
+
+            def run(self):
+                shortest_path = None
+                all_time_shortest_path = ("placeholder", np.inf)
+                for i in range(self.n_iterations):
+                    all_paths = self.gen_all_paths()
+                    self.spread_pheronome(
+                        all_paths, self.n_best, shortest_path=shortest_path)
+                    shortest_path = min(all_paths, key=lambda x: x[1])
+                    if shortest_path[1] < all_time_shortest_path[1]:
+                        all_time_shortest_path = shortest_path
+                    self.pheromone *= self.decay
+                return self.route_conversion(all_time_shortest_path[0]), all_time_shortest_path[1]
+
+            def route_conversion(self, lst):
+                result = [*lst[0]]
+                for i in range(1, len(lst)):
+                    result.append(lst[i][1])
+                return result
+
+            def spread_pheronome(self, all_paths, n_best, shortest_path):
+                sorted_paths = sorted(all_paths, key=lambda x: x[1])
+                for path, dist in sorted_paths[:n_best]:
+                    for move in path:
+                        self.pheromone[move] += 1.0 / self.distances[move]
+
+            def gen_path_dist(self, path):
+                total_dist = 0
+                for ele in path:
+                    total_dist += self.distances[ele]
+                return total_dist
+
+            def gen_all_paths(self):
+                all_paths = []
+                for i in range(self.n_ants):
+                    path = self.gen_path(0)
+                    all_paths.append((path, self.gen_path_dist(path)))
+                return all_paths
+
+            def gen_path(self, start):
+                path = []
+                visited = set()
+                visited.add(start)
+                prev = start
+                for i in range(len(self.distances) - 1):
+                    move = self.pick_move(
+                        self.pheromone[prev], self.distances[prev], visited)
+                    path.append((prev, move))
+                    prev = move
+                    visited.add(move)
+                path.append((prev, start))
+                return path
+
+            def pick_move(self, pheromone, dist, visited):
+                pheromone = np.copy(pheromone)
+                pheromone[list(visited)] = 0
+                row = pheromone ** self.alpha * ((1.0 / dist) ** self.beta)
+                norm_row = row / row.sum()
+                move = np_choice(self.all_inds, 1, p=norm_row)[0]
+                return move
+
+        return AntColony(self.matrix, n_ants, n_best, n_iterations, decay, alpha, beta).run()
---
+++ fapmethods/IntApr.py
@@ -0,0 +1,309 @@
+import numpy
+import sympy
+import numpy as np
+import scipy.optimize
+
+
+def chebishevPoints(x_values, y_values, n):
+    """
+    Функция, рассчитывающая значения в точках функции Чебышева
+    :params x_values: массив точек x
+    :params y_values: массив точек y
+    :return: значения в точках функции
+    """
+    a = min(x_values)
+    b = max(y_values)
+    result = []
+    for i in range(1, n+1):
+        result.append(0.5*(a+b) + 0.5*(b-a)*numpy.cos((2*i-1)/2/n*numpy.pi))
+    return result
+
+
+def lagrx(x_values, y_values):
+    """
+    функция, вызывающая интерполяцию методом Лагранжа
+    :params x_values: массив точек x
+    :params y_values: массив точек y
+    :return: массив точек и интерполяционный член Лагранжа
+    """
+    ratio = (max(x_values)-min(x_values))/len(x_values)
+    if len(x_values) >= 60 and ratio < 1:
+        result = lagr_cheb(x_values, y_values)
+        if result is None:
+            return lagrx_standart(x_values, y_values)
+        return result
+    else:
+        return lagrx_standart(x_values, y_values)
+
+
+def lagrx_standart(x_values, y_values):
+    """
+    Функция, выполняющая интерполяцию методом Лагранжа для всех точек
+    :params x_values: массив точек x
+    :params y_values: массив точек y
+    :return: массив точек x,y и интерполяционный член Лагранжа
+    """
+    x0 = sympy.Symbol('x')
+    s = 0
+    for i in range(len(x_values)):
+        t1 = 1
+        t2 = 1
+        for j in range(len(x_values)):
+            t1 *= x0-x_values[j] if i != j else 1
+            t2 *= x_values[i] - x_values[j] if i != j else 1
+        s += y_values[i]*(t1/t2)
+    return x_values, y_values, [sympy.simplify(s).subs(x0, x_values[i]) for i in x_values]
+
+
+def lagr_cheb(x_values, y_values):
+    """
+    Функция, выполняющая интерполяцию методом Лагранжа для Чебышева
+    :params x_values: массив точек x
+    :params y_values: массив точек y
+    """
+    x0 = sympy.Symbol('x')
+
+    rounded_x = [round(i, 1) for i in x_values]
+    last_poly = None
+    last_error = None
+
+    for power in range(3, 50):
+        xx = list(set(rounded_x) & set(round(i, 1)
+                  for i in chebishevPoints(x_values, y_values, power)))
+        s = 0
+        if len(xx) != power:
+            continue
+
+        for i in range(len(xx)):
+            t1 = 1
+            t2 = 1
+            for j in range(len(xx)):
+                t1 *= x0-xx[j] if i != j else 1
+                t2 *= xx[i] - xx[j] if i != j else 1
+            s += y_values[rounded_x.index(xx[i])]*(t1/t2)
+
+        error = lagr_error(x_values, y_values, sympy.lambdify(x0, s))
+        if last_error is not None and error > last_error*2:
+            return x_values, y_values, [last_poly.subs(x0, x_values[i]) for i in x_values]
+        if last_error is None or last_error > error:
+            last_error = error
+            last_poly = s
+
+    if last_poly is not None:
+        return x_values, y_values, [last_poly.subs(x0, x_values[i]) for i in x_values]
+    return last_poly
+
+
+def lagr_error(x_values, y_values, poly):
+    """
+    Функция для расчета Taylor remainder theorem
+    :params x_values: массив точек x
+    :params y_values: массив точек y
+    :params poly: Ваш полином для расчета
+    :return: найденная точка
+    """
+    maxx = 0
+    for i, x in enumerate(x_values):
+        m = abs(y_values[i]-poly(x))
+        if m > maxx:
+            maxx = m
+    return maxx
+
+
+def newtons_interpolation(x_values, y_values, is_forward=True):
+    """
+    Функция для интерполяции методом Ньютона
+    :params x_values: массив точек x
+    :params y_values: массив точек y
+    :params is_forward: интерполяция вперед или назад
+    :return: массив точек x, y, интерполяционный многочлен Ньютона
+    """
+    x0 = sympy.symbols('x')
+    n_res = len(x_values)
+    interpol = []
+
+    def poly_newton_coefficient(x, y):
+        """
+        Функция для определения коэффициентов
+        :params x: x
+        :params y: y
+        :return: коэффициент a
+        """
+        m = len(x)
+        x = np.copy(x)
+        a = np.copy(y)
+        h = x[1] - x[0]
+        for k in range(1, m):
+            a[k:m] = (a[k:m] - a[k - 1]) / h
+        return a
+
+    def newton_first(x, y):
+        """
+        Первый полином Ньютона
+        :params x: x
+        :params y: y
+        :return: result of многочлена Ньютона "вперед"
+        """
+        x_res = sympy.symbols('x')
+        a = poly_newton_coefficient(x, y)
+        result = 0
+        for i in range(len(a)):
+            factor = a[i]
+            for j in range(i):
+                factor *= (x_res - x[j])
+            result += factor
+        return sympy.simplify(result)
+
+    def newton_second(x, y):
+        """
+        Второй полином Ньютона
+        :params x: x
+        :params y: y
+        :return: result of многочлена Ньютона "назад"
+        """
+        x_res = sympy.symbols('x')
+        a = poly_newton_coefficient(x[::-1], y[::-1])
+        n = len(a)
+        result = 0
+        for i in range(n):
+            factor = a[i]
+            for j in range(n - 1, n - 1 - i, -1):
+                factor *= (x_res - x[j])
+            result += factor
+        return sympy.simplify(result)
+
+    if is_forward:
+        res = newton_first(x_values, y_values)
+        interpol = [res.subs(x0, x_values[i]) for i in range(n_res)]
+
+    elif is_forward is False:
+        res = newton_second(x_values, y_values)
+        interpol = [res.subs(x0, x_values[i]) for i in range(n_res)]
+
+    return x_values, y_values, interpol
+
+
+def linear_function_approximation(x_values, y_values):
+    """
+    Функция для аппроксимации линейной функцией
+    :params x_values: массив точек x
+    :params y_values: массив точек y
+    :return: массив точек x,y и вид аппроксимирующей функции
+    """
+    n = len(x_values)
+    xx = sympy.Symbol('x')
+    s1, s2, s3, s4 = 0, 0, 0, 0
+    for i in range(n):
+        s1 += x_values[i] ** 2
+        s2 += x_values[i]
+        s3 += x_values[i] * y_values[i]
+        s4 += y_values[i]
+
+    c1 = (s3 * n - s2 * s4) / (s1 * n - s2 ** 2)
+    c0 = (s1 * s4 - s2 * s3) / (s1 * n - s2 ** 2)
+    yy = sympy.simplify(c0 + c1 * xx)
+
+    f_values = [yy.subs(xx, x_values[i]) for i in range(n)]
+
+    return x_values, y_values, f_values
+
+
+def quadratic_function_approximation(x_values, y_values):
+    """
+    Функция для аппроксимации квадратичной функцией
+    :params x_values: массив точек x
+    :params y_values: массив точек y
+    :return: массив точек x,y и вид аппроксимирующей функции
+    """
+    n = len(x_values)
+    xx = sympy.Symbol('x')
+    s1, s2, s3, s4, s5, s6, s7 = 0, 0, 0, 0, 0, 0, 0
+    for i in range(n):
+        s1 += x_values[i] ** 4
+        s2 += x_values[i] ** 3
+        s3 += x_values[i] ** 2
+        s4 += x_values[i]
+        s5 += x_values[i] ** 2 * y_values[i]
+        s6 += x_values[i] * y_values[i]
+        s7 += y_values[i]
+
+    a = [[0] * 3 for _ in range(3)]
+    b = [0] * 3
+    a[0][0], a[0][1], a[0][2] = s1, s2, s3
+    a[1][0], a[1][1], a[1][2] = s2, s3, s4
+    a[2][0], a[2][1], a[2][2] = s3, s4, n
+    b[0], b[1], b[2] = s5, s6, s7
+
+    c2, c1, c0 = list(np.linalg.solve(a, b))
+    yy = sympy.simplify(c0 + c1 * xx + c2 * xx ** 2)
+
+    f_values = [yy.subs(xx, x_values[i]) for i in range(n)]
+
+    return x_values, y_values, f_values
+
+
+def normal_distribution_approximation(x_values, y_values):
+    """
+    Функция для аппроксимации квадратичной функцией
+    :params x_values: массив точек x
+    :params y_values: массив точек y
+    :return: массив точек x,y и вид аппроксимирующей функции
+    """
+    n = len(x_values)
+
+    def t3(value):
+        return s.subs({a: value[0], b: value[1], c: value[2]})
+
+    x, y, a, b, c = sympy.symbols('x y a b c')
+    expr = (y - a * sympy.exp(-(x - b) ** 2 / (2 * c ** 2))) ** 2
+
+    s = sympy.sympify(0)
+    my = max(y_values)
+    mx = x_values[y_values.index(my)]
+    length = abs(max(x_values) - min(x_values))
+
+    for i in range(len(x_values)):
+        s += expr.subs({x: x_values[i], y: y_values[i]})
+
+    bnds = ((-np.inf, np.inf), (-np.inf, np.inf), (0, None))
+    res = scipy.optimize.minimize(t3, [my, mx, length], bounds=bnds)
+
+    yy = sympy.simplify(
+        res.x[0] * sympy.exp(-(x - res.x[1]) ** 2 / (2 * res.x[2] ** 2)))
+    f_values = [yy.subs(x, x_values[i]) for i in range(n)]
+
+    return x_values, y_values, f_values
+
+
+def interpolate(x_values, y_values, method="lagr"):
+    """
+    Функция, позволяющая выбрать необходимый метод для интерполяции
+    :params x_values: массив точек x
+    :params y_values: массив точек y
+    :params method: выбранный метод
+    """
+    if method == "lagr":
+        return lagrx(x_values=x_values, y_values=y_values)
+    elif method == "newton1":
+        return newtons_interpolation(x_values=x_values, y_values=y_values, is_forward=True)
+    elif method == "newton2":
+        return newtons_interpolation(x_values=x_values, y_values=y_values, is_forward=False)
+    else:
+        raise "Неверный метод"
+
+
+def approximate(x_values, y_values, method='quadratic'):
+    """
+    Функция, позволяющая выбрать необходимый метод для аппроксимации
+    :params x_values: массив точек x
+    :params y_values: массив точек y
+    :params method: выбранный метод
+    """
+    if method == "linear":
+        return linear_function_approximation(x_values=x_values, y_values=y_values)
+    elif method == "quadratic":
+        return quadratic_function_approximation(x_values=x_values, y_values=y_values)
+    elif method == "newton2":
+        return normal_distribution_approximation(x_values=x_values, y_values=y_values)
+    else:
+        raise "Неверный метод"
